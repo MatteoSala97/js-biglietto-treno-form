bd464ff95c5e5d2ba5fbfa1563ef879d8235aecd
MatteoSala97 <matteo111997@gmail.com>
2024-01-08 15:52:27 +0100

    html css js config

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+//Variables
+let 
+kmAmountElement = document.getElementById ("kmAmount"), 
+psgAgeElement = document.getElementById ("psgAge")
+//Const
+const kmPrice = 0.21;
+const discountUnder = 20
+const discountOver = 40
+
+// kmAmount = 40
+// psgAge = 19
+
+let kmAmount, psgAge;
+
+let calcButton = document.getElementById 
+("submitValues");
+
+let resetButton = document.getElementById("reset-all")
+
+let result = document.getElementById 
+("result");
+
+
+
+
+//Function allowing numeric values to be put into input areas
+
+function getValues(){
+    
+//Assigning variables values based on input areas numeric results
+
+    kmAmount = kmAmountElement.value;
+
+    psgAge = psgAgeElement.value;
+
+    console.log (kmAmount, psgAge)
+}
+
+
+let finalPrice 
+
+// function that calculates the discount amount
+
+function priceCalc (firstElement, secondElement){
+    let fullPrice = firstElement*kmPrice
+
+    return fullPrice - (fullPrice * secondElement / 100)
+}
+
+
+//Function that checks the passenger's age
+
+function finalPriceCalc(){
+    if (psgAge < 18) {
+        finalPrice = priceCalc (kmAmount, discountUnder);
+    }   
+    else if (psgAge > 65){
+        finalPrice = priceCalc (kmAmount, discountOver);
+    }   
+    else {
+        finalPrice = kmAmount * kmPrice;
+    }
+}
+
+// Method allowing to submit values for price calculation
+
+calcButton.addEventListener("click", function (e){
+    getValues()
+    finalPriceCalc()
+    console.log (finalPrice)
+    result.innerHTML = "Il prezzo del tuo biglietto è di "+ " " + finalPrice.toFixed(2) + "€"
+})
+
+//Method allowing to reset all the values
+
+resetButton.addEventListener("click", function (e){
+    kmAmountElement.value = 0
+    psgAgeElement.value = 0
+    result.innerHTML = ""
+})
+
+
+
